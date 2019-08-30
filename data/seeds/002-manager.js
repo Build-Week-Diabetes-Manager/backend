2fb830127fca7857ae887a85aa0e91a79cd79f62
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-08-29 23:57:17 -0700

    add probs

--- data/seeds/002-manager.js
+++ data/seeds/002-manager.js
@@ -5,12 +5,12 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('manage').insert([
-        { timestamp: '2019-10-10 10:12', code: 59, value: 33.21, user_id: 1 },
-        { timestamp: '2019-10-10 10:12', code: 59, value: 33.21, user_id: 1 },
-        { timestamp: '2019-10-10 10:12', code: 55, value: 33.21, user_id: 1 },
-        { timestamp: '2019-10-10 10:12', code: 59, value: 33.21, user_id: 2 },
-        { timestamp: '2019-10-10 10:12', code: 59, value: 33.21, user_id: 2 },
-        { timestamp: '2019-10-10 10:12', code: 55, value: 33.21, user_id: 3 }
+        { timestamp: '2019-10-10 10:12', code: 59, value: 33, user_id: 1 },
+        { timestamp: '2019-10-10 10:12', code: 59, value: 33, user_id: 1 },
+        { timestamp: '2019-10-10 10:12', code: 55, value: 33, user_id: 1 },
+        { timestamp: '2019-10-10 10:12', code: 59, value: 33, user_id: 2 },
+        { timestamp: '2019-10-10 10:12', code: 59, value: 33, user_id: 2 },
+        { timestamp: '2019-10-10 10:12', code: 55, value: 33, user_id: 3 }
       ]);
     });
 };
